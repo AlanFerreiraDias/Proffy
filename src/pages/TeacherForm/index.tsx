c8e445808adf47629faa85dde4ec0546a6844a2c
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-09 15:15:09 -0300

    Create teacher form inputs

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
+
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css'
 
 function TeacherForm() {
     return (
-        <PageHeader title="Que legal que você quer dar aulas.">
+        <div id="page-teacher-form" className="container">
+            <PageHeader
+                title="Que legal que você quer dar aulas."
+                description="O primeiro passo é preencher o formulário de inscrição."
+            >
+                <main>
+                    <fieldset>
+                        <legend>Seus dados</legend>
+                        <Input name="name" label="Nome completo" />
+                        <Input name="avatar" label="Avatar" />
+                        <Input name="whatsapp" label="Whatsapp" />
+                    </fieldset>
 
-        </PageHeader>
+                    <fieldset>
+                        <legend>Sobre a aula</legend>
+                        <Input name="subject" label="Matéria" />
+                        <Input name="cost" label="Custo da sua aula por hora." />
+                    </fieldset>
+
+                    <footer>
+                        <p>
+                            <img src={warningIcon} alt="Aviso Importante" />
+                            Importante! <br />
+                            Preencha todos os dados
+                        </p>
+                        <button type="button">
+                            Salvar cadastro
+                        </button>
+                    </footer>
+                </main>
+            </PageHeader>
+        </div >
     )
 }
 
