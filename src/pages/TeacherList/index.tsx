cb69eb6908436a6ffd9d38a93a96febd17799f28
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-09 15:14:28 -0300

    substitute divs for input component

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -5,26 +5,16 @@ import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
+import Input from '../../components/Input';
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Esses são os Proffys disponíveis." >
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matérias</label>
-                        <input type="text" id="subject" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="subject">Dia da Semana</label>
-                        <input type="text" id="subject" />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="subject">Hora</label>
-                        <input type="text" id="subject" />
-                    </div>
+                    <Input name="subject" label="Matéria" />
+                    <Input name="week_day" label="Dia da Semana" />
+                    <Input type="time" name="time" label="Hora" />
 
                 </form>
             </PageHeader>
